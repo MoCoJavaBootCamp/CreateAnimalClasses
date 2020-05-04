709b3bb8ea9142c026570d85fd3bca9f92184230
azdoolittle <drewdoolittle@gmail.com>
2020-05-04 12:05:52 -0400

    pasted comments from README to AnimalApp

--- src/AnimalApp.java
+++ src/AnimalApp.java
@@ -1,3 +1,16 @@
+/*
+Four new animals were created using the Animal Class.
+
+Each animal inherited sleep and sleep methods from its parent class, but were modified.
+
+The encapsulation isn't very strong due to no private variables.
+
+However, methods were overridden to cater to the exact subclass.
+
+This is called polymorphism, where the subclass takes on a new form.
+
+ */
+
 public class AnimalApp {
 
     public static void main(String[] args) {
